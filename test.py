4570f1ad1716b604971cd9331ca890427421c314
Edwike12 <nyaunchoedwike@gmail.com>
2021-12-07 14:03:18 +0300

    creates a TestUser subclass that inherits testcase from  unnittset

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import unittest # Importing the unittest module
-from User import User
+from User import User  # Importing the contact class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
 
