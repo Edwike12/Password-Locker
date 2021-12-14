4d31928a74c00180325f533bc039f198f9da26f4
Edwike12 <nyaunchoedwike@gmail.com>
2021-12-14 10:38:45 +0300

    created a function user login in main.py

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ def  create_new_user(username,password):
     new_user=User(username,password)
     return new_user
 
-def save_user(User):
-    '''
-    its a function to save user
-    '''
-    User.save_user()
+    def save_user(User):
+        '''
+        its a function to save user
+        '''
+        User.save_user()
+
+    def login_user(username,password):
+        """
+        function that checks whether a user exist and then login the user in.
+        """
+        check_user = Credentials.verify_user(username,password)
+        return check_user
 
 def create_new_credentials(account,username,password):
     '''
@@ -25,4 +32,6 @@ def save_credentials(Credentials):
     its a function to save credentials
     '''
     Credentials.save_credentials() 
+
+
      
